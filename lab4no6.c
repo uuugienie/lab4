69cf585b6380c0ee266e760c0d1ba95c76140af2
6504062610331 Usanee <s6504062610331@email.kmutnb.ac.th>
2025-12-23 15:22:58 +0700

    display square of a character

---
+++ lab4no6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int c=0,r=0,i=0,j=0;
+	char s;
+	scanf("%d %d",&c,&r);
+ 
+    for(i=0;i<r;i++)
+	{
+		for(j=0;j<r;j++)
+		{
+			printf("%d",c);
+		}
+		printf("\n");
+	}
+}
